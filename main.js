835c31ddc9f5b5de38c3e0c7da16ccc190e9fcd9
Roi Bendet <roib@roi-b-mbp15.local>
2018-03-11 18:07:03 +0200

    added some features: 1.save to localstore feature 2.saving changes

--- main.js
+++ main.js
@@ -1,48 +1,60 @@
+let appData = [];
+
+function appStarter() {
+  const myItems = JSON.parse(localStorage.getItem('myItems'));
+  if (myItems === null) {
+  }
+  if (myItems) {
+    appData = myItems;
+    createList();
+  }
+}
+
+function createList() {
+  const list = document.getElementById('list');
+  list.innerHTML = '';
+  appData.forEach((item) => {
+    list.innerHTML += createItem(item);
+  })
+
+}
+
+function createItem(item) {
+  const isChecked = item.isChecked;
+  return `<li id=${item.id}><label><input type="checkbox" ${isChecked ? 'checked' : ''} onchange="checkBoxOnChange(this)"/> ${item.text}</label></li>`;
+}
+
 function addAnItem() {
   const inputValue = document.getElementById('inputField');
   if (inputValue.value.length === 0) {
     return console.log('input field is empty');
   }
   else {
-
-    const list = document.getElementById('list');
-    const liElm = document.createElement('li');
-    const labelElm = document.createElement('label');
-    const checkboxElm = document.createElement('input');
-    labelElm.innerText = inputValue.value;
-
-    checkboxElm.onchange = checkBoxOnChange;
-    checkboxElm.type = "checkbox";
-    // why ?
-    const notWorking = `<li><label><input type="checkbox"/>${inputValue.value}</label></li>`;
-
-    // labelElm.appendChild(checkboxElm);
-    // liElm.appendChild(labelElm);
-    // list.appendChild(liElm);
-    list.innerHTML += notWorking;
+    console.log(inputValue.value);
+    const item = {
+      id: Math.floor(Math.random() * 900000) + 10000,
+      text: inputValue.value,
+      isChecked: false
+    };
+    appData.push(item);
     document.getElementById('inputField').value = '';
     addItemsToLocalStorage()
   }
-  // list.appendChild(test);
 }
 
 function addItemsToLocalStorage() {
-
-  const nodelist = document.querySelectorAll('li');
-  const nodelistArr = Array.from(nodelist);
-  const listToSave = nodelistArr.map((item) => {
-    const text = item.innerText;
-    const isChecked = item.querySelector('input').checked;
-    return {text, isChecked}
-  });
-  localStorage.setItem('myItems', JSON.stringify(listToSave));
-}
-
-function printLocalStorage() {
-  const myItems = JSON.parse(localStorage.getItem('myItems'));
-  console.log(myItems);
+  localStorage.setItem('myItems', JSON.stringify(appData));
+  createList();
 }
 
 function checkBoxOnChange(event) {
-  console.log('onChange', event.target.checked);
+  let liElement = event.closest('li');
+  appData.forEach((item) => {
+    if (item.id === Number(liElement.id)) {
+      item.isChecked = item.isChecked ? false : true;
+    }
+  });
+  addItemsToLocalStorage();
 }
+
+appStarter();
