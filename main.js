a2b7795546d3304bd05e564942c7d0bddcb42409
Roi Bendet <roib@roi-b-mbp15.local>
2018-03-13 15:25:39 +0200

    sort feature added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let appData = [];
+const list = document.getElementById('list');
 
 function appStarter() {
   const myItems = JSON.parse(localStorage.getItem('myItems'));
@@ -11,7 +12,6 @@ function appStarter() {
 }
 
 function createList() {
-  const list = document.getElementById('list');
   list.innerHTML = '';
   appData.forEach((item) => {
     list.innerHTML += createItem(item);
@@ -21,7 +21,7 @@ function createList() {
 
 function createItem(item) {
   const isChecked = item.isChecked;
-  return `<li id=${item.id}><label><input type="checkbox" ${isChecked ? 'checked' : ''} onchange="checkBoxOnChange(this)"/> ${item.text}</label></li>`;
+  return `<li id=${item.id}><label><input type="checkbox" ${isChecked ? 'checked' : ''} onchange="checkBoxOnChange(this)"/> ${item.text}</label><span>${item.date}</span></li>`;
 }
 
 function addAnItem() {
@@ -30,31 +30,59 @@ function addAnItem() {
     return console.log('input field is empty');
   }
   else {
-    console.log(inputValue.value);
     const item = {
       id: Math.floor(Math.random() * 900000) + 10000,
       text: inputValue.value,
-      isChecked: false
+      isChecked: false,
+      date: new Date()
     };
+    const liElement = document.createElement('li');
+    const labelElement = `<label><input type="checkbox" ${item.isChecked ? 'checked' : ''} onchange="checkBoxOnChange(this)"/> ${item.text}</label><span>${item.date}</span>`;
+    liElement.setAttribute("id", item.id);
+    liElement.innerHTML = labelElement;
     appData.push(item);
     document.getElementById('inputField').value = '';
+    list.appendChild(liElement);
     addItemsToLocalStorage()
   }
 }
 
 function addItemsToLocalStorage() {
   localStorage.setItem('myItems', JSON.stringify(appData));
-  createList();
 }
 
 function checkBoxOnChange(event) {
   let liElement = event.closest('li');
   appData.forEach((item) => {
     if (item.id === Number(liElement.id)) {
-      item.isChecked = item.isChecked ? false : true;
+      item.isChecked = !item.isChecked;
     }
   });
   addItemsToLocalStorage();
+}
+
+function sortList(event) {
+  const selectedItemValue = event.value;
+  if (selectedItemValue === 'date') {
+    appData.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    })
+  }
+  if (selectedItemValue === 'alphabet') {
+    appData.sort((a, b) => {
+      if (a.text < b.text) return -1;
+      if (a.text > b.text) return 1;
+      return 0;
+    })
+  }
+  if (selectedItemValue === 'done') {
+    appData.sort((a, b) => {
+      return (a.isChecked === b.isChecked) ? 0 : a.isChecked ? -1 : 1;
+    })
+
+  }
+  createList();
+
 }
 
 appStarter();
