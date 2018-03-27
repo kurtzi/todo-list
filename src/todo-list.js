895a80cd3c36d75d567cb5ec59679653a364ec79
Ido Inbar <idoi@ido-i-mbp15.local>
2018-03-27 14:45:12 +0300

    Added checkboxes. Checking a checkbox also saves the data to local storage.

--- src/todo-list.js
+++ src/todo-list.js
@@ -4,14 +4,13 @@
 
 var itemArr = [];
 
-
 function addEntry() {
+    console.log("addEntry is running")
     var newEntry = document.getElementsByTagName("input")[0].value
     if(newEntry) {
         var currTime = new Date();
         document.getElementsByTagName("input")[0].value = '';
-        itemArr.push([newEntry, currTime])
-
+        itemArr.push([newEntry, currTime, false])
         updateList();
     }
     else {
@@ -19,17 +18,59 @@ function addEntry() {
     }
 }
 
+function getSavedList() {
+    console.log("getSavedList is running")
+    var savedList = JSON.parse(localStorage.getItem("entriesList"))
+    if (savedList) {
+        itemArr = savedList;
+         updateList()
+    }
+}
 
 function updateList() {
+    console.log("updateList is running")
     var todoList = document.getElementById("todoList");
     while( todoList.firstChild ){
         todoList.removeChild( todoList.firstChild );
     }
-    for (var item in itemArr) {
+    console.log("before for loop")
+    for (var thisItem in itemArr) {
 
         var node = document.createElement("LI");
-        var textnode = document.createTextNode(itemArr[item][0]);
-        node.appendChild(textnode);
+        var textNode = document.createTextNode(itemArr[thisItem][0]);
+        var checkBox = document.createElement('input');
+
+        checkBox.type = 'checkbox';
+        checkBox.id = 'isDone' + thisItem;
+        checkBox.checked =  itemArr[thisItem][2];
+        checkBox.addEventListener("click", function() {
+            changeIsDone(this.id)
+        });
+
+        node.appendChild(checkBox);
+        node.appendChild(textNode);
+
         todoList.appendChild(node);
     }
+    saveListLocally();
+}
+
+function changeIsDone(d) {
+    console.log("changeIsDone is running")
+
+
+    var isChecked = document.getElementById(d).checked
+    var index = d.substr(6)  //gets the index #
+    itemArr[index][2] = isChecked;
+    saveListLocally();
+    console.log('checkbox changed', d, isChecked, index)
+}
+
+function saveListLocally() {
+    console.log("before setItem")
+    try {localStorage.setItem("entriesList", JSON.stringify(itemArr))
+        console.log("after setitem")}
+    catch (err){
+        console.log(err)
+    };
 }
