ea1e02d50947cac2f03f38ced67b845222a4db50
Ido Inbar <idoi@ido-i-mbp15.local>
2018-04-02 13:28:12 +0300

    Added edit and remove feature. Sorting is now persistent. Changed all getElementBy to querySelector.

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,18 +1,15 @@
-/**
- * Created by idoi on 25/03/2018.
- */
-
-let itemArr = [];
+let itemObj = [];
+let isEditBoxVis = false;
 
 function addEntry() {
-    let newEntry = document.getElementById("inputBox").value
-    console.log(newEntry)
+    let newEntry = document.querySelector("#inputBox").value;
     if(newEntry) {
         let currTime = new Date();
-        document.getElementById("inputBox").value = '';
-        itemArr.push({text: newEntry, time: currTime, isDone: false})
+        document.querySelector("#inputBox").value = '';
+        itemObj.push({text: newEntry, time: currTime, isDone: false});
         updateList();
         sortList();
+        attachEditRemoveBtns();
     }
     else {
         alert('Please enter something, surely you got SOMETHING to do')
@@ -20,161 +17,193 @@ function addEntry() {
 }
 
 function getSavedList() {
-    let savedList = JSON.parse(localStorage.getItem("entriesList"))
+    let savedList = JSON.parse(localStorage.getItem("entriesList"));
+    let sortNum = localStorage.getItem("sorting");
     if (savedList) {
-        itemArr = savedList;
-         updateList()
+        itemObj = savedList.map(item => ({text: item.text, time: new Date(item.time), isDone:item.isDone}));
+         updateList();
+        document.querySelector('#sortBox').value = Number(sortNum);
     }
 }
 
 function updateList() {
-    //console.log("updateList is running")
-    let todoList = document.getElementById("todoList");
+    let todoList = document.querySelector("#todoList");
     while( todoList.firstChild ){
         todoList.removeChild( todoList.firstChild );
     }
-    for (let thisItem in itemArr) {
-
-        let node = document.createElement("LI");
-        let textNode = document.createTextNode(itemArr[thisItem]['text']);
+    for (let thisItem in itemObj) {
+        let node = document.createElement("div");
+        node.id = "contBox" + thisItem;
+        let textNode = document.createTextNode(itemObj[thisItem]['text']);
         let checkBox = document.createElement('input');
-
+        let btnNode = document.createElement("div");
+        let editIcon = document.createElement('i');
+        editIcon.classList.add("fa", "fa-pencil");
+        editIcon.id = "editBtn" + thisItem;
+        let delIcon = document.createElement('i');
+        delIcon.classList.add("fa", "fa-close");
+        delIcon.id = "delBtn" + thisItem;
         checkBox.type = 'checkbox';
         checkBox.id = 'isDone' + thisItem;
-        checkBox.checked =  itemArr[thisItem].isDone;
+        checkBox.checked =  itemObj[thisItem].isDone;
         checkBox.addEventListener("click", function() {
-            changeIsDone(this.id)
+            changeIsDone(this.id);
         });
 
+        btnNode.style = 'display: none';
+        btnNode.classList.add('editBox');
+        btnNode.appendChild(delIcon);
+        btnNode.appendChild(editIcon);
+        node.appendChild(btnNode);
         node.appendChild(checkBox);
         node.appendChild(textNode);
 
         todoList.appendChild(node);
     }
-
+    isEditBoxVis = true;
+    toggleEdit();
     saveListLocally();
+    attachEditRemoveBtns();
 }
 
+function toggleEdit() {
+
+    if (isEditBoxVis) {
+        document.querySelectorAll('div.editBox').forEach(item => item.style = "display: none");
+        document.querySelector('#editBtn').innerText  = 'Edit/Remove';
+        isEditBoxVis = false;
+    }
+    else  {
+        document.querySelectorAll('div.editBox').forEach(item => item.style = "display: inline-block");
+        document.querySelector('#editBtn').innerText = 'Done'
+        isEditBoxVis = true;
+    }
+}
 function changeIsDone(d) {
-    let isChecked = document.getElementById(d).checked
-    let index = d.substr(6)  //gets the index #
-    itemArr[index].isDone = isChecked;
+    let isChecked = document.querySelector(`#${d}`).checked;
+    let index = d.substr(6);  //gets the index #
+    itemObj[index].isDone = isChecked;
     deSelectDropdown();
     saveListLocally();
 }
+
 function deSelectDropdown () {
-    document.getElementById('sortBox').value = -1;
-}
-function saveListLocally() {
-    try {localStorage.setItem("entriesList", JSON.stringify(itemArr))
-        }
-    catch (err){
-        console.log(err)
-    };
+    document.querySelector('#sortBox').value = -1;
 }
 
-function removeDone() {
-    let newArr = [];
-    for (let i = 0;  i < itemArr.length; i++) {
-        if (!itemArr[i].isDone) {
-            newArr.push(itemArr[i]);
+function saveListLocally() {
+    try {localStorage.setItem("entriesList", JSON.stringify(itemObj))
+    localStorage.setItem("sorting", document.querySelector('#sortBox').value)
         }
+    catch (err){
+        console.log(err);
     }
-    itemArr = newArr;
+}
+
+function removeEntry(index) {
+    if (confirm("Are you sure?")) {
+        var element = document.querySelector(`#contBox${index}`);
+        element.parentNode.removeChild(element);
+        itemObj.splice(index, 1);
+        saveListLocally();
+    } else {
+
+    }
+}
+
+function editEntry(index) {
+    let editedText = prompt("Please edit the entry:", itemObj[index].text);
+    if (editedText === null || editedText === itemObj[index].text ) {
+        //cancel
+    }
+    else {
+        itemObj[index].text = editedText;
+        document.querySelector("#contBox" + index).childNodes[2].data = editedText;
+        saveListLocally();
+    }
+
+
+}
+
+function sortList() {
+    let selection = Number(document.querySelector('#sortBox').value);
+    if (selection === 0) {
+        itemObj.sort(function(a, b) {
+            return a.time < b.time ? -1 : a.time > b.time ? 1 : 0;
+        })
+    }
+    if (selection === 1) {
+        itemObj.sort(function(a, b) {
+            return a.time > b.time ? -1 : a.time < b.time ? 1 : 0;
+        })
+    }
+    if (selection === 2 ) {
+        itemObj.sort(function(a, b) {
+            return a.isDone > b.isDone ? -1 : a.isDone < b.isDone ? 1 : 0;
+        })
+    }
+    if (selection === 3) {
+        itemObj.sort(function(a, b) {
+            return a.isDone < b.isDone ? -1 : a.isDone > b.isDone ? 1 : 0;
+        })
+    }
+    if (selection === 4) {
+        itemObj.sort(function(a, b) {
+        let text1 = a.text.toUpperCase();
+        let text2 = b.text.toUpperCase();
+        return text1 < text2 ? -1 : text1 > text2 ? 1 : 0;
+        })
+    }
+    if (selection === 5) {
+        itemObj.sort(function(a, b) {
+        let text1 = a.text.toUpperCase();
+        let text2 = b.text.toUpperCase();
+        return text1 > text2 ? -1 : text1 < text2 ? 1 : 0;
+        })
+    }
     updateList();
 }
 
-<!--0 date oldest to newest-->
-<!--1 date newest oldest-->
-<!--2 done-->
-<!--3 not done-->
-<!--4 alpha A to Z-->
-<!--5 alpha Z to A-->
+window.onload = function() {
+    init();
+};
 
+function init() {
+    getSavedList();
+    document.querySelector("#sortBox").addEventListener("change", function(){
+        sortList();
+    });
+    document.querySelector("#editBtn").addEventListener("click", function(){
+        toggleEdit();
+    });
+    document.querySelector("#addBtn").addEventListener("click", function(){
+        addEntry();
+    });
+    document.querySelector("#inputBox").addEventListener("keypress", function(event) {
+        if ((event.which || event.keyCode) === 13) {
+            addEntry();
+        }
+    });
+}
 
-function sortList() {
-    let selection = document.getElementById('sortBox').value;
-    switch(Number(selection)) {
-        case 0:
-            itemArr.sort(function(a, b) {
+function attachEditRemoveBtns() {
+    document.querySelectorAll('i.fa-pencil').forEach(item => {
+        if (!item.classList.contains('tagged')) {
+            item.addEventListener("click", function(){
+                let curIndex = this.id.substr(7);
+                editEntry(curIndex)
+            });
+            item.classList.add("tagged")
+        }
+    });
 
-                if (a.time < b.time) {
-                    return -1;
-                }
-                if (a.time > b.time) {
-                    return 1;
-                }
-                return 0;
-            })
-            break;
-        case 1:
-            itemArr.sort(function(a, b) {
-
-                if (a.time > b.time) {
-                    return -1;
-                }
-                if (a.time < b.time) {
-                    return 1;
-                }
-                return 0;
-            })
-            break;
-        case 2:
-            itemArr.sort(function(a, b) {
-
-                if (a.isDone > b.isDone) {
-                    return -1;
-                }
-                if (a.isDone < b.isDone) {
-                    return 1;
-                }
-                return 0;
-            })
-            break;
-        case 3:
-            itemArr.sort(function(a, b) {
-
-                if (a.isDone < b.isDone) {
-                    return -1;
-                }
-                if (a.isDone > b.isDone) {
-                    return 1;
-                }
-                return 0;
-            })
-            break;
-        case 4:
-            itemArr.sort(function(a, b) {
-                let text1 = a.text.toUpperCase();
-                let text2 = b.text.toUpperCase();
-                if (text1 < text2) {
-                    return -1;
-                }
-                if (text1 > text2) {
-                    return 1;
-                }
-                return 0;
-            })
-            break;
-        case 5:
-            itemArr.sort(function(a, b) {
-                let text1 = a.text.toUpperCase();
-                let text2 = b.text.toUpperCase();
-                if (text1 > text2) {
-                    return -1;
-                }
-                if (text1 < text2) {
-                    return 1;
-                }
-                return 0;
-            })
-
-            break;
-
-        default:
-            console.log('Wrong selection')
-            break;
-
-    }
-    updateList()
+    document.querySelectorAll('i.fa-close').forEach(item => {
+        if (!item.classList.contains('tagged')) {
+            item.addEventListener("click", function () {
+                let curIndex = this.id.substr(6);
+                removeEntry(curIndex);
+            });
+            item.classList.add("tagged")
+        }
+    })
 }
