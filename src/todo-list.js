26a5c5ac2c39ef8366521f546547402c8c2e1bd4
Ido Inbar <idoi@ido-i-mbp15.lan>
2018-04-10 18:57:21 +0300

    List is not re-rendered when adding an enrty. Sorting is kept when adding/editing/removing

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,15 +1,19 @@
-let itemObj = [];
+let allTasks = [];
 let isEditBoxVis = false;
+let globalId;
+let idToFind;
+let todoList;
 
 function addEntry() {
-    let newEntry = document.querySelector("#inputBox").value;
-    if(newEntry) {
-        let currTime = new Date();
+    const newEntry = document.querySelector("#inputBox").value;
+    if (newEntry) {
         document.querySelector("#inputBox").value = '';
-        itemObj.push({text: newEntry, time: currTime, isDone: false});
-        updateList();
+        const newId = getId();
+        const newItem = {text: newEntry, time: new Date(), isDone: false, id: newId};
+        allTasks.push(newItem);
         sortList();
-        attachEditRemoveBtns();
+        todoList.insertBefore(createNode(newItem), todoList.childNodes[getIndexNumber(newId)]);
+        saveDataLocally();
     }
     else {
         alert('Please enter something, surely you got SOMETHING to do')
@@ -17,193 +21,167 @@ function addEntry() {
 }
 
 function getSavedList() {
-    let savedList = JSON.parse(localStorage.getItem("entriesList"));
-    let sortNum = localStorage.getItem("sorting");
+    const savedList = JSON.parse(localStorage.getItem("entriesList"));
+    const sortNum = localStorage.getItem("sorting");
+    globalId = localStorage.getItem("globalId") || 5000;
     if (savedList) {
-        itemObj = savedList.map(item => ({text: item.text, time: new Date(item.time), isDone:item.isDone}));
-         updateList();
+        allTasks = savedList.map(item => ({text: item.text, time: new Date(item.time), isDone: item.isDone, id: item.id}));
+        updateList();
         document.querySelector('#sortBox').value = Number(sortNum);
     }
 }
 
+function getId() {
+    return globalId++
+}
+
 function updateList() {
-    let todoList = document.querySelector("#todoList");
-    while( todoList.firstChild ){
-        todoList.removeChild( todoList.firstChild );
-    }
-    for (let thisItem in itemObj) {
-        let node = document.createElement("div");
-        node.id = "contBox" + thisItem;
-        let textNode = document.createTextNode(itemObj[thisItem]['text']);
-        let checkBox = document.createElement('input');
-        let btnNode = document.createElement("div");
-        let editIcon = document.createElement('i');
-        editIcon.classList.add("fa", "fa-pencil");
-        editIcon.id = "editBtn" + thisItem;
-        let delIcon = document.createElement('i');
-        delIcon.classList.add("fa", "fa-close");
-        delIcon.id = "delBtn" + thisItem;
-        checkBox.type = 'checkbox';
-        checkBox.id = 'isDone' + thisItem;
-        checkBox.checked =  itemObj[thisItem].isDone;
-        checkBox.addEventListener("click", function() {
-            changeIsDone(this.id);
-        });
+    todoList.innerHTML = '';
+    allTasks.forEach(item => {
+        todoList.appendChild(createNode(item));
+    });
+}
 
-        btnNode.style = 'display: none';
-        btnNode.classList.add('editBox');
-        btnNode.appendChild(delIcon);
-        btnNode.appendChild(editIcon);
-        node.appendChild(btnNode);
-        node.appendChild(checkBox);
-        node.appendChild(textNode);
-
-        todoList.appendChild(node);
+function createNode(item) {
+    let node = document.createElement("li");
+    node.id = "contBox" + item.id;
+    let checked = '';
+    if (item.isDone) {
+        checked = 'checked';
     }
-    isEditBoxVis = true;
-    toggleEdit();
-    saveListLocally();
-    attachEditRemoveBtns();
+    const curDis = isEditBoxVis ? "inline-block" : "none";
+    node.innerHTML = `<div class="editBox" style="display: ${curDis}"><i class="fa fa-close" id="delBtn${item.id}"></i><i class="fa fa-pencil" id="editBtn${item.id}"></i></div><input type="checkbox" id="isDone${item.id}" ${checked}>${item['text']}`;
+    node.querySelector(`#isDone${item.id}`).addEventListener("click", function () { changeIsDone(`${item.id}`)});
+    node.querySelector(`i.fa-pencil`).addEventListener("click", function () { editEntry(`${item.id}`)});
+    node.querySelector(`i.fa-close`).addEventListener("click", function () { removeEntry(`${item.id}`)});
+    return node;
 }
 
 function toggleEdit() {
-
     if (isEditBoxVis) {
-        document.querySelectorAll('div.editBox').forEach(item => item.style = "display: none");
-        document.querySelector('#editBtn').innerText  = 'Edit/Remove';
+        document.querySelectorAll('div.editBox').forEach(item => item.style.display =  "none");
+        document.querySelector('#editBtn').innerText = 'Edit/Remove';
         isEditBoxVis = false;
     }
-    else  {
-        document.querySelectorAll('div.editBox').forEach(item => item.style = "display: inline-block");
-        document.querySelector('#editBtn').innerText = 'Done'
+    else {
+        document.querySelectorAll('div.editBox').forEach(item => item.style.display = "inline-block");
+        document.querySelector('#editBtn').innerText = 'Done';
         isEditBoxVis = true;
     }
 }
+
+function compareId(e) {
+    return Number(e.id) === Number(idToFind);
+}
+
+function getIndexNumber(id) {
+    idToFind = id;
+    return allTasks.findIndex(compareId);
+}
+
 function changeIsDone(d) {
-    let isChecked = document.querySelector(`#${d}`).checked;
-    let index = d.substr(6);  //gets the index #
-    itemObj[index].isDone = isChecked;
-    deSelectDropdown();
-    saveListLocally();
+    const entryIndex = getIndexNumber(d);
+    allTasks[entryIndex].isDone = document.querySelector(`#isDone${d}`).checked;
+    sortList();
+    updateList();
+    saveDataLocally();
 }
 
-function deSelectDropdown () {
-    document.querySelector('#sortBox').value = -1;
-}
-
-function saveListLocally() {
-    try {localStorage.setItem("entriesList", JSON.stringify(itemObj))
-    localStorage.setItem("sorting", document.querySelector('#sortBox').value)
-        }
-    catch (err){
-        console.log(err);
+function saveDataLocally() {
+    try {
+        localStorage.setItem("entriesList", JSON.stringify(allTasks));
+        localStorage.setItem("sorting", document.querySelector('#sortBox').value);
+        localStorage.setItem("globalId", globalId)
+    }
+    catch (err) {
+        console.error(err);
     }
 }
 
 function removeEntry(index) {
     if (confirm("Are you sure?")) {
-        var element = document.querySelector(`#contBox${index}`);
+        const element = document.querySelector(`#contBox${index}`);
         element.parentNode.removeChild(element);
-        itemObj.splice(index, 1);
-        saveListLocally();
-    } else {
-
+        allTasks.splice(getIndexNumber(index), 1);
+        saveDataLocally();
     }
 }
 
-function editEntry(index) {
-    let editedText = prompt("Please edit the entry:", itemObj[index].text);
-    if (editedText === null || editedText === itemObj[index].text ) {
-        //cancel
+function editEntry(id) {
+    const index = getIndexNumber(id);
+    const editedText = prompt("Please edit the entry:", allTasks[index].text);
+    if (editedText !== null && editedText !== allTasks[index].text) {
+        allTasks[index].text = editedText;
+        let curBox = document.querySelector("#contBox" + id);
+        curBox.childNodes[2].data = editedText;
+        const selection = Number(document.querySelector('#sortBox').value);
+        if (selection === 4 || selection === 5 ) {
+            const editedEntry = allTasks[index];
+            curBox.parentNode.removeChild(curBox);
+            sortList();
+            todoList.insertBefore(createNode(editedEntry), todoList.childNodes[getIndexNumber(id)]);
+        }
+        saveDataLocally();
     }
-    else {
-        itemObj[index].text = editedText;
-        document.querySelector("#contBox" + index).childNodes[2].data = editedText;
-        saveListLocally();
-    }
-
-
 }
 
 function sortList() {
-    let selection = Number(document.querySelector('#sortBox').value);
+    const selection = Number(document.querySelector('#sortBox').value);
     if (selection === 0) {
-        itemObj.sort(function(a, b) {
+        allTasks.sort(function (a, b) {
             return a.time < b.time ? -1 : a.time > b.time ? 1 : 0;
         })
     }
     if (selection === 1) {
-        itemObj.sort(function(a, b) {
+        allTasks.sort(function (a, b) {
             return a.time > b.time ? -1 : a.time < b.time ? 1 : 0;
         })
     }
-    if (selection === 2 ) {
-        itemObj.sort(function(a, b) {
+    if (selection === 2) {
+        allTasks.sort(function (a, b) {
             return a.isDone > b.isDone ? -1 : a.isDone < b.isDone ? 1 : 0;
         })
     }
     if (selection === 3) {
-        itemObj.sort(function(a, b) {
+        allTasks.sort(function (a, b) {
             return a.isDone < b.isDone ? -1 : a.isDone > b.isDone ? 1 : 0;
         })
     }
     if (selection === 4) {
-        itemObj.sort(function(a, b) {
-        let text1 = a.text.toUpperCase();
-        let text2 = b.text.toUpperCase();
-        return text1 < text2 ? -1 : text1 > text2 ? 1 : 0;
+        allTasks.sort(function (a, b) {
+            const text1 = a.text.toUpperCase();
+            const text2 = b.text.toUpperCase();
+            return text1 < text2 ? -1 : text1 > text2 ? 1 : 0;
         })
     }
     if (selection === 5) {
-        itemObj.sort(function(a, b) {
-        let text1 = a.text.toUpperCase();
-        let text2 = b.text.toUpperCase();
-        return text1 > text2 ? -1 : text1 < text2 ? 1 : 0;
+        allTasks.sort(function (a, b) {
+            const text1 = a.text.toUpperCase();
+            const text2 = b.text.toUpperCase();
+            return text1 > text2 ? -1 : text1 < text2 ? 1 : 0;
         })
     }
-    updateList();
 }
 
-window.onload = function() {
-    init();
-};
+init();
 
 function init() {
+    todoList = document.querySelector("#todoList");
     getSavedList();
-    document.querySelector("#sortBox").addEventListener("change", function(){
+    document.querySelector("#sortBox").addEventListener("change", function () {
         sortList();
+        updateList();
+        saveDataLocally();
     });
-    document.querySelector("#editBtn").addEventListener("click", function(){
+    document.querySelector("#editBtn").addEventListener("click", function () {
         toggleEdit();
     });
-    document.querySelector("#addBtn").addEventListener("click", function(){
+    document.querySelector("#addBtn").addEventListener("click", function () {
         addEntry();
     });
-    document.querySelector("#inputBox").addEventListener("keypress", function(event) {
+    document.querySelector("#inputBox").addEventListener("keypress", function (event) {
         if ((event.which || event.keyCode) === 13) {
             addEntry();
         }
     });
-}
-
-function attachEditRemoveBtns() {
-    document.querySelectorAll('i.fa-pencil').forEach(item => {
-        if (!item.classList.contains('tagged')) {
-            item.addEventListener("click", function(){
-                let curIndex = this.id.substr(7);
-                editEntry(curIndex)
-            });
-            item.classList.add("tagged")
-        }
-    });
-
-    document.querySelectorAll('i.fa-close').forEach(item => {
-        if (!item.classList.contains('tagged')) {
-            item.addEventListener("click", function () {
-                let curIndex = this.id.substr(6);
-                removeEntry(curIndex);
-            });
-            item.classList.add("tagged")
-        }
-    })
 }
